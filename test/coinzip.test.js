5604e0045d5c7eb48c080c35f9d91ce15bf9a188
Mark Sargento <marksargento.00@gmail.com>
2019-02-21 20:23:23 +0800

    Create test for public#getTickers()

--- test/coinzip.test.js
+++ test/coinzip.test.js
@@ -1,6 +1,12 @@
 import test from 'ava';
 import Coinzip from '../lib/coinzip';
 
+test('Throws an error if version passed is invalid', t => {
+  const version = 'v4';
+  const error = t.throws(() => new Coinzip({version}), Error);
+  t.is(error.message, `version ${version} not supported`);
+});
+
 test('Check if apiUrl is defined', t => {
   t.is(Coinzip.apiUrl(), 'https://www.coinzip.co/api');
 });
@@ -10,5 +16,6 @@ test('Intialize a new Coinzip instance without parameters', t => {
 
   t.is(coinzip.version, 'v2');
   t.is(coinzip.getVersion(), 'v2');
+  t.truthy(coinzip.getVersion);
   t.truthy(coinzip.getBasePath);
 });
---
+++ test/coinzip/public.test.js
@@ -0,0 +1,49 @@
+import test from 'ava';
+import Public from '../../lib/coinzip/public';
+
+const MOCK_BASE_PATH = 'https://mock.coinzip.com/api/v2';
+const ERROR = '404 - "404 Not Found"';
+const pub = new Public(MOCK_BASE_PATH);
+
+test('Make sure all methods are defined', t => {
+  t.truthy(pub.getTickers);
+});
+
+/**
+ * Make Sure all methods are Promise-ified.
+ * No need make an actual http requests
+ * This is not actually necessary but I just put it for documentation purposes.
+ **/
+
+test('getTickers() returns an error if market is undefined or missing', async t => {
+  const error = await t.throwsAsync(() => pub.getTickers(), Error);
+
+  t.is(error.message, 'Cannot destructure property `market` of \'undefined\' or \'null\'.');
+});
+
+test('getTickers() returns error if market doesn\'t exists', async t => {
+  pub.request = () => Promise.reject(new Error(ERROR));
+
+  const error = await t.throwsAsync(() => pub.getTickers({market: 'non-existing-market'}));
+
+  t.is(error.message, ERROR);
+});
+
+test('getTickers returns proper response', async t => {
+  const GET_TICKER_RESPONSE = {
+    at: 1550749357,
+    ticker: {
+      buy: '0.0',
+      sell: '0.0',
+      low: '0.03387009',
+      high: '0.03397093',
+      last: '0.03387009',
+      vol: '0.0'
+    }
+  };
+  pub.request = () => Promise.resolve(GET_TICKER_RESPONSE);
+
+  const resp = await pub.getTickers({market: 'bchbtc'});
+
+  t.is(resp, GET_TICKER_RESPONSE);
+});
